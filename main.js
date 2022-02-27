53abeb07ca87fa738c5056aff489c8c2c823962f
QuickStrike123 <73514740+QuickStrike123@users.noreply.github.com>
2022-02-27 09:59:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+function setup() {
+
+    canvas = createCanvas(490, 490);
+
+    canvas.position(550, 250);
+
+    Video = createCapture(VIDEO);
+
+    Video.hide();
+
+}
+
+function draw() {
+
+    image(Video, 0, 0, 490, 490);
+
+    stroke("lime");
+
+    fill("skyblue");
+
+    //top
+
+    circle(0, 0, 80);
+
+    stroke("blue");
+
+    fill("red");
+
+    circle(240, 0, 80);
+
+    stroke("lime");
+
+    fill("skyblue");
+
+    circle(490, 0, 80);
+
+    //left
+
+    stroke("blue");
+
+    fill("red");
+
+    circle(0, 240, 80);
+
+    stroke("lime");
+
+    fill("skyblue");
+
+    circle(0, 480, 80);
+
+    //bottom
+
+    stroke("blue");
+
+    fill("red");
+
+    circle(240, 490, 80)
+
+    stroke("lime");
+
+    fill("skyblue");
+
+    circle(490, 490, 80)
+
+    //right
+
+    stroke("blue");
+
+    fill("red");
+
+    circle(490, 240, 80)
+
+}
+
+function TakeSnapshot() {
+
+    save("img.png");
+
+}
